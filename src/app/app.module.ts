0896a5662fae1bc43017dafbc97b5076c50224ef
peter <peter@soez.io>
2018-04-20 11:39:45 +0800

    添加路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  RouterModule,
+  Router,
+  Routes
+} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { MeComponent } from './me/me.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'me', component: MeComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +26,8 @@ import { MeComponent } from './me/me.component';
     MeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
