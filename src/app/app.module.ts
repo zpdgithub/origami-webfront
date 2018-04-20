a88953a84d75a39b47b61ecbae2ea22e88b2a62b
peter <peter@soez.io>
2018-04-20 11:34:29 +0800

    添加me组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { MeComponent } from './me/me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    MeComponent
   ],
   imports: [
     BrowserModule
